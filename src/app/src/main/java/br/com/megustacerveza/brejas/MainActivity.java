5a6bf54142dead4ceab56314ca0f48fd4300537f
Antonio Fernando S. Ladeia <antonio@reconcavotecnologia.org.br>
2016-09-22 17:51:26 -0300

    refactor get data from fields

--- src/app/src/main/java/br/com/megustacerveza/brejas/MainActivity.java
+++ src/app/src/main/java/br/com/megustacerveza/brejas/MainActivity.java
@@ -9,41 +9,64 @@ import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText litrao;
+    private EditText tradicional;
+    private EditText bigLatao;
+    private EditText latao;
+    private EditText longNeck;
+    private EditText lata;
+    private EditText safadinha;
+    private EditText piriguete;
+    private EditText otherVol;
+    private EditText otherPrice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        litrao = (EditText) findViewById(R.id.litrao);
+        tradicional = (EditText) findViewById(R.id.tradicional);
+        bigLatao = (EditText) findViewById(R.id.bigLatao);
+        latao = (EditText) findViewById(R.id.latao);
+        longNeck = (EditText) findViewById(R.id.longNeck);
+        lata = (EditText) findViewById(R.id.lata);
+        safadinha = (EditText) findViewById(R.id.safadinha);
+        piriguete = (EditText) findViewById(R.id.piriguete);
+        otherVol = (EditText) findViewById(R.id.otherVol);
+        otherPrice = (EditText) findViewById(R.id.otherPrice);
+
         setContentView(R.layout.activity_main);
     }
 
     public void evaluate(View view) {
-        EditText litrao = (EditText) findViewById(R.id.litrao);
+
         BigDecimal valueLitrao = new BigDecimal(litrao.getText().toString());
 
-        EditText tradicional = (EditText) findViewById(R.id.tradicional);
+
         BigDecimal valueTradicional = new BigDecimal(tradicional.getText().toString());
 
-        EditText bigLatao = (EditText) findViewById(R.id.bigLatao);
+
         BigDecimal valueBigLatao = new BigDecimal(bigLatao.getText().toString());
 
-        EditText latao = (EditText) findViewById(R.id.latao);
+
         BigDecimal valueLatao = new BigDecimal(latao.getText().toString());
 
-        EditText longNeck = (EditText) findViewById(R.id.longNeck);
+
         BigDecimal valueLongNeck = new BigDecimal(longNeck.getText().toString());
 
-        EditText lata = (EditText) findViewById(R.id.lata);
+
         BigDecimal valueLata = new BigDecimal(lata.getText().toString());
 
-        EditText safadinha = (EditText) findViewById(R.id.safadinha);
+
         BigDecimal valueSafadinha = new BigDecimal(safadinha.getText().toString());
 
-        EditText piriguete = (EditText) findViewById(R.id.piriguete);
+
         BigDecimal valuePiriguete = new BigDecimal(piriguete.getText().toString());
 
-        EditText otherVol = (EditText) findViewById(R.id.otherVol);
+
         BigDecimal valueOtherVol = new BigDecimal(otherVol.getText().toString());
 
-        EditText otherPrice = (EditText) findViewById(R.id.otherPrice);
+
         BigDecimal valueOtherPrice = new BigDecimal(otherPrice.getText().toString());
     }
 }
